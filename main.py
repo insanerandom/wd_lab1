e153523b80b6a23e1ecca44a527124eca3a1bf9b
insanerandom <98644506+insanerandom@users.noreply.github.com>
2022-03-12 00:45:48 +0100

    Add files via upload
    Dodanie zadań 6, 7, 8 i 9

--- main.py
+++ main.py
@@ -89,3 +89,27 @@ capString = string.capitalize()
 string = "sZymanowski"
 capString_ = string.capitalize()
 print(capString + " " + capString_)
+print("\nZad6")
+string_ = "Tonight's the night we're gonna make it happen Tonight we'll put all other things aside Get in this time and show me some affection We're going for those pleasures in the night I want to love you, feel you Wrap myself around you I want to squeeze you, please you I just can't get enough And if you move real slow I'll let it go I'm so excited and I just can't hide it I'm about to lose control and I think I like it I'm so excited and I just can't hide it And I know, I know, I know, I know I know I want you We shouldn't even think about tomorrow Sweet memories will last for long, long time We'll have a good time, baby Don't do worry And if we're still playing around boy That's just fine Let's get excited We just can't hide it I'm about to lose control and I think I like it I'm so excited and I just can't hide it I know, I know, I know, I know I know I want you, I want you I want to love you, Feel you, wrap myself around you I want to squeeze you, please you No, I just can't get enough And if you move real slow I'll let it go I'm so excited I just can't hide it I'm about to lose control And I think I like it I'm so excited And I just can't hide it I know, I know, I know, I know I know I want you, Want you Look what you do to me You've got me burning up How did you get to me I've got to give it up Baby, I'm so excited I just can't hide it I'm about to lose control And I think I like it I'm so excited I can't deny, no, no I know, I know I want you"
+x = string_.count("excited")
+print("wystapienia slowa 'excited': %(x)d" % {'x': x})
+print("\nZad7")
+string = "Przemyslaw"
+print(len(string))
+print("Druga litera stringu: %(x)s" % {'x': string[1]})
+print("Ostatnia litera stringu: %(x)s" % {'x': string[len(string) - 1]})
+print("\nZad8")
+x = string_.split("I")
+print(x)
+print("wyraz pierwszy: '%(x)s'" % {'x': x[0]})
+print("wyraz drugi: '%(x)s'" % {'x': x[1]})
+print("wyraz trzeci: '%(x)s'" % {'x': x[2]})
+print("\nZad9")
+string = "tekst"
+n1 = 1.6180339887
+n1 = float(n1)
+n2 = 143
+n2 = hex(n2)
+print(string)
+print(n1)
+print(n2)
